99683d3b798dec5e10611a23f5abcb990ad8698a
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-10-04 01:12:00 +0700

    Update p2_msg_sender.sol

--- chap2/p2_msg_sender.sol
+++ chap2/p2_msg_sender.sol
@@ -29,3 +29,40 @@ Trong ví dụ đơn giản này, bất kỳ ai cũng có thể gọi setMyNumbe
 
 Sử dụng msg.sender cung cấp cho bạn tính bảo mật của blockchain Ethereum — cách duy nhất để ai đó có thể sửa đổi dữ liệu của người khác là phải đánh cắp khóa riêng liên kết với địa chỉ Ethereum của họ.
 */
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id]=msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
