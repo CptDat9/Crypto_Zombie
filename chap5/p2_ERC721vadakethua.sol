fd3f8df560b77009349b560c022f2bb04af47329
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-10-10 10:09:38 +0700

    Update p2_ERC721vadakethua.sol

--- chap5/p2_ERC721vadakethua.sol
+++ chap5/p2_ERC721vadakethua.sol
@@ -36,7 +36,7 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombieattack.sol";
 // Import file here
 import "./erc721.sol";
-// Declare ERC721 inheritance here
-contract ZombieOwnership is ZombieAttack, ERC721 {
+
+contract ZombieOwnership is ZombieAttack, ERC721 //Đa kế thừa dùng thêm dấu "," thôi. {
 
 }
