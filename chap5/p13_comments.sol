2e8d928e5c23f3bed9c00d952d22199b895bc2f9
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-10-11 09:43:27 +0700

    Create p13_comments.sol

---
+++ chap5/p13_comments.sol
@@ -0,0 +1,6 @@
+@title — E.g. A contract that manages transferring zombie ownership
+
+@author — Your name!
+
+@dev — E.g. Compliant with OpenZeppelin's implementation of the ERC721 spec draft
+-----------
